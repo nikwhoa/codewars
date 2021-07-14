098b472b93d58e91eec6119cdda83137ae9491fe
Nikita <norenkonikita@gmail.com>
2021-07-14 18:50:40 +0300

    Count the smiley faces!

--- Count the smiley faces!/script.js
+++ Count the smiley faces!/script.js
@@ -1,17 +1,56 @@
-
 function countSmileys(arr) {
   let smile = 0;
-    arr.forEach(element => {
-        if (element === ':)' || element === ':D' || element === ';-D' || element === ':~)') {
-            smile++;
-            return smile;
-        } else {
-          console.log('not face');
-        }
-    });
-    console.log(smile);
+  let eyes = [':', ';'];
+  let nose = ['-', '~'];
+  let mouth = [')', 'D'];
+  let newArr = [];
+  let array = [];
+
+  arr.forEach((element, i) => {
+    if (element[0][0] === eyes[0] || element[0][0] === eyes[1]) {
+      array[i] = element;
+      newArr = array.filter(function (el) {
+        return el !== null;
+      });
+      return newArr;
+    }
+  });
+
+
+
+  newArr.forEach(el => {
+    if (el.length > 2) {
+      if (el[1] === nose[0] || el[1] === nose[1]) {
+        return newArr;
+      } else {
+        newArr.splice(newArr.indexOf(el), 1);
+        return newArr;
+      }
+    }
+  });
+
+
+
+  newArr.forEach(el => {
+    if (el[1] === mouth[0] || el[1] === mouth[1]) {
+      return newArr;
+    } 
+    // else {
+    //   newArr.splice(newArr.indexOf(el), 1);
+    //   return newArr;
+    // }
+  });
+
+
+
+
+  let sum = newArr.length;
+  console.log(newArr);
+  // console.log(sum);
+  return sum;
+
+
 }
 
-countSmileys([':D',':~)',';~D',':)']);
 
-
+countSmileys([':)', ':(', ':D', ':O', ':;']);
