da439aff755dd50c2b0000f00ad77175f1ee254e
Nikita <norenkonikita@gmail.com>
2021-07-22 12:17:23 +0300

    Two fighters, one winner. DONE

--- Two fighters, one winner/script.js
+++ Two fighters, one winner/script.js
@@ -1,20 +1,20 @@
-class Fighter {
-    constructor(name, health, damagePerAttack) {
-        this.name = name;
-        this.health = health;
-        this.damagePerAttack = damagePerAttack;
-    }
-}
+
 
 function declareWinner(fighter1, fighter2, firstAtacker) {
+    class Fighter {
+        constructor(name, health, damagePerAttack) {
+            this.name = name;
+            this.health = health;
+            this.damagePerAttack = damagePerAttack;
+        }
+    }
     let fighterOne = fighter1;
     let fighterTwo = fighter2;
-
+    let winner;
+    
     if (firstAtacker === fighterOne.name) {
-       
         attack(fighterOne, fighterTwo);
     } else {
-        
         attack(fighterTwo, fighterOne);
     }
 
@@ -23,10 +23,41 @@ function declareWinner(fighter1, fighter2, firstAtacker) {
         let attackerOneHealth = attackerOne.health,
             attackerTwoHealth = attackerTwo.health,
             attackerOneDamage = attackerOne.damagePerAttack,
-            attackerTwoDamage = attackerTwo.damagePerAttack;
+            attackerTwoDamage = attackerTwo.damagePerAttack,
+            counter = 0;
+            
 
+            while (attackerOneHealth > 0 || attackerTwoHealth > 0) {            
+                if (counter) {
+                    attackerOneHealth -= attackerTwoDamage;
+                    console.log(attackerTwo.name  + ' has ' + attackerTwoHealth);
+                    counter--;
+                } else {
+                    attackerTwoHealth -= attackerOneDamage;
+                    console.log(attackerOne.name + ' has ' + attackerOneHealth);
+                    counter++;
+                }
+
+                if (attackerOneHealth <= 0 || attackerTwoHealth <= 0) {
+                    break;
+                }
+
+               
+
+            }
+            if (attackerTwoHealth > 0) {
+                console.log('The winner is ' + attackerTwo.name);
+                winner = attackerTwo.name;
+            } 
+            if (attackerOneHealth > 0) {
+                console.log('The winner is ' + attackerOne.name);
+                winner = attackerOne.name;
+            }
+            
+            // console.log(attackerOneHealth, attackerTwoHealth);
     }
-
+    return winner;
 }
 
-declareWinner(new Fighter("Lew", 5, 10), new Fighter('Nikita', 100, 5), 'Nikita');
+// declareWinner(new Fighter("Yarik", 40, 10), new Fighter('Nikita', 100, 5), 'Yarik');
+declareWinner(new Fighter("Yarik", 20000, 500), new Fighter('Serega', 10, 1), 'Yarik');
