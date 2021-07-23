7ad80b06bdb62f383893dce5e2de7fcd430dec91
Nikita <norenkonikita@gmail.com>
2021-07-23 13:01:47 +0300

    Summing a number's digits

--- Summing a number's digits/script.js
+++ Summing a number's digits/script.js
@@ -1,24 +1,25 @@
 function sumDigits(number) {
-    if (number < 0) {
-     let z = number.toString().slice(1);
-     let f = z[0];
-     let g = z[1];
-     let q = +f;
-     let w = +g;
-        console.log(q+w);
-     return q + w;
-    } else {
-
-        let a = number.toString();
-        let b = a[0];
-        let c = a[1];
-        let d = +b;
-        let e = +c;
-        console.log(d+e)
-        return d + e;
-    }
     
+    let n = number.toString();
+    let arrayOfNumbers = [];
+    let result = 0;
+    for (let i = 0; i < n.length; i++) {
+        arrayOfNumbers[i] = n[i];   
+    }
+
+    if (arrayOfNumbers[0] === '-') {
+        arrayOfNumbers.shift();
+    }
+
+    let a = [];
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        a[i] = parseInt(arrayOfNumbers[i]);
+    }
+
+    a.forEach(el => {console.log(result); return result += el;});
+    
+    return result;
     
 }
 
-sumDigits(-32);
+sumDigits(-327);
